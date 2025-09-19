4d394bc2da9e4bf4efda6b973a7189370a5af2ea
Kaspars Dzerkals <a242728kd@rvt.lv>
2025-09-19 13:27:36 +0300

    Average of three numbers NEEDS MANUAL INPUT!

--- Avg.java
+++ Avg.java
@@ -1,17 +1,18 @@
+import java.util.Scanner;
+
 public class Avg {
     public static void main(String[] args){
-        //Dotie skaitļi
-        double sk1 = 8;
-        double sk2 = 2;
-        double sk3 = 3;
+        Scanner ievade = new Scanner(System.in);
 
-        //Rezultāta aprēķināšana
-        double result = (sk1 + sk2 + sk3) / 3;
+        System.out.println("Input your 1st number: ");
+        double sk1 = ievade.nextDouble();
 
-        //Rezultāta izvade 1 variants
-        System.out.println(result);
+        System.out.println("Input your 2nd number: ");
+        double sk2 = ievade.nextDouble();
 
-        //Rezultāta aprēķināšana un izvade bez liekas rindiņas
+        System.out.println("Input your 3rd number: ");
+        double sk3 = ievade.nextDouble();
+
         System.out.println((sk1 + sk2 + sk3) / 3);
     }
 }
