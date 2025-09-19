104a567b8efc5380975e2955d1a7f1c44d87f181
Kaspars Dzerkals <a242728kd@rvt.lv>
2025-09-19 12:46:43 +0300

    Pievienoju 'for' loop kas aizskaita no 1 - 5.

--- App.java
+++ App.java
@@ -4,7 +4,10 @@ public class App {
     public static void main(String[] args) {
         // Your code goes here...
         System.out.println(123);
-        //check
+
+        for (int i = 1; i < 6; i++){
+            System.out.println(i + "\n");
+        }
     }
 }
 
