d78ee1ae15f9d9ac881cdbaef4b7a6c634e92d04
Kaspars Dzerkals <a242728kd@rvt.lv>
2025-10-31 09:44:21 +0200

    Basic scanner saving string

---
+++ src/main/java/rvt/Helloworl.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Helloworl {
+    public static void main(String[] args){
+        Scanner inputReader = new Scanner(System.in);
+
+        String message = inputReader.nextLine();
+        //stem
+        System.out.println(message);
+    }
+}
+
