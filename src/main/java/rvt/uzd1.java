a7b485e3b9ad2e0e88aadbf4cb8d8cf221ec1464
Kaspars Dzerkals <a242728kd@rvt.lv>
2025-10-31 09:48:07 +0200

    For I in range print every item from list

---
+++ src/main/java/rvt/uzd1.java
@@ -0,0 +1,10 @@
+package rvt;
+
+public class uzd1 {
+    public static void main(String[] args){
+        int[] skaitli = {6, 8, 10, 2, 6};
+        for (int i = 0; i < skaitli.length; i++){
+            System.out.println(skaitli[i]);
+        }
+    }
+}
