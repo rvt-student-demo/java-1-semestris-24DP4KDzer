26407577efa3d079438c4d7f296bf6628465c109
Kaspars Dzerkals <a242728kd@rvt.lv>
2025-10-31 09:42:32 +0200

    for loop in java

---
+++ src/main/java/rvt/ForLoop.java
@@ -0,0 +1,20 @@
+package rvt;
+public class ForLoop {
+    public static void main(String[] args){
+        //stem
+        
+        int number = 1;
+        while(number <= 6){
+            System.out.println(number);
+            number += 1;
+        }
+
+        System.out.println();
+
+
+        for (int i = 0; i < 10; i++){
+            System.out.println(i + 1);
+        
+        }
+    }
+}
