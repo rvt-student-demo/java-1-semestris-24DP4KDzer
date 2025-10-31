464e54de994a8f055b88389b977a9b31513c26c7
Kaspars Dzerkals <a242728kd@rvt.lv>
2025-10-31 09:45:52 +0200

    OnceUponaTime

---
+++ src/main/java/rvt/OnceUopn.java
@@ -0,0 +1,10 @@
+public class OnceUopn {
+    public static void main(String[] args){
+        System.err.println("Once upon a time");
+        System.err.println("There was");
+        System.err.println("A program!");
+    for (int i = 1; i <= 100; i = i + 1){
+        System.out.println(i);
+    }
+}
+}
