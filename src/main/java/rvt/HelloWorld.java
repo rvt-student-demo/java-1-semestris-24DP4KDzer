6d1eb6f64d4241aeaadcdb5d975afdba9b50428b
Kaspars Dzerkals <a242728kd@rvt.lv>
2025-10-31 09:45:05 +0200

    Print "HelloWorld"

---
+++ src/main/java/rvt/HelloWorld.java
@@ -0,0 +1,15 @@
+/* { - Atver bloku ; } - Aizverbloku */
+//stem
+
+public class HelloWorld {
+    // Programmas sākuma punkts
+    // Program entry point
+    // funkcija == metode(method)
+    // main - funkcija == obligata
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+       
+    }
+  
+}
+
