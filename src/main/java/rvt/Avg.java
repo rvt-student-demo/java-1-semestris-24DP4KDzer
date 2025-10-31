3c5bdccdfb230139c2c38b6733a0dfa04aa04f07
Kaspars Dzerkals <a242728kd@rvt.lv>
2025-10-31 09:38:14 +0200

    Avarage

---
+++ src/main/java/rvt/Avg.java
@@ -0,0 +1,19 @@
+package rvt;
+import java.util.Scanner;
+
+public class Avg {
+    public static void main(String[] args){
+        Scanner ievade = new Scanner(System.in);
+
+        System.out.println("Input your 1st number: ");
+        double sk1 = ievade.nextDouble();
+
+        System.out.println("Input your 2nd number: ");
+        double sk2 = ievade.nextDouble();
+
+        System.out.println("Input your 3rd number: ");
+        double sk3 = ievade.nextDouble();
+
+        System.out.println((sk1 + sk2 + sk3) / 3);
+    }
+}
