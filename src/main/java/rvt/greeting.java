544ae090321607fd55ea7e2724a771f4f64b4fe9
Kaspars Dzerkals <a242728kd@rvt.lv>
2025-10-31 09:43:35 +0200

    Scanner + String

---
+++ src/main/java/rvt/greeting.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class greeting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Whats your name?: ");
+
+        String message = scanner.nextLine();
+
+        System.out.println("Hi " + message + "!");
+    }
+}
