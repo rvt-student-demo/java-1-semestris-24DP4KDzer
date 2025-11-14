7a0c8b91785c0c7d2e50e3a9ff146474a5413a3d
Kaspars Dzerkals <a242728kd@rvt.lv>
2025-11-14 10:46:50 +0200

    Part 1 Statistics izdarīts

---
+++ src/main/java/rvt/Statistics.java
@@ -0,0 +1,18 @@
+package rvt;
+
+public class Statistics {
+    private int count;
+    
+    public Statistics(){
+
+    }
+
+    public void addNumber(int number){
+
+    }
+
+    public int getCount() {
+
+    }
+    
+}
