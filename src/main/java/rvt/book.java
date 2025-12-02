4f67c18e56ec7f8c32c1af87b883cf3d2f093385
Kaspars Dzerkals <a242728kd@rvt.lv>
2025-12-02 09:46:22 +0200

    Pievienoju 'Books' Strād

--- src/main/java/rvt/book.java
+++ src/main/java/rvt/book.java
@@ -3,15 +3,23 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class book {
+
+    public static void printer(){
+        System.out.print("What do you want to print?\n'1'= Title\n'2'= Pages\n'3'= Year\n'everything'= everything\n>>> ");
+
+    }
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
         ArrayList<String> name = new ArrayList<>();
         ArrayList<Integer> lapas = new ArrayList<>();
         ArrayList<Integer> gads = new ArrayList<>();
+       
         
+ 
         System.out.print("Input Book Title: ");
         String title = scanner.nextLine();
+
         name.add(title);
 
         System.out.print("Input Book Pages: ");
@@ -23,8 +31,25 @@ public class book {
         gads.add(year);
 
 
-        System.out.println("Names: " + name);
-        System.out.println("Pages: " + lapas);
-        System.out.println("Years: " + gads);
+
+        printer();
+
+        String choice = scanner.nextLine();
+
+        if (choice.equals("1")) {
+            System.out.println(name);
+        } else if (choice.equals("2")) {
+            System.out.println(lapas);
+        } else if (choice.equals("3")) {
+            System.out.println(gads);
+        } else if (choice.equals("everything")) {
+            System.out.println("Titles: " + name);
+            System.out.println("Pages: " + lapas);
+            System.out.println("Years: " + gads);
+        } else {
+            System.out.println("Invalid input.");
+        }
+
+    
     }
 }
